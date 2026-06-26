66d94b1f8bce43276ef78ebca78ba681abb6556c
dsolerazz <diesolsaenz@gmail.com>
2026-06-25 18:19:51 -0600

    Primer ejercicio Modulo C corregido

--- Caso3_DeltaNull/ModuloC/3_realityWeaving.py
+++ Caso3_DeltaNull/ModuloC/3_realityWeaving.py
@@ -37,19 +37,20 @@ import base64
 # Puedes modificar el código a partir de aquí:
 
 def solution(entrada):
-    s1, s2 = entrada
-    if len(s1) > len(s2):
-        minimo = len(s2)
-        no_minimo = s1
+    s1, s2 = entrada #Estas son las entradas, que es la tupla de dos strings dados
+    if len(s1) > len(s2): #Se comparan las longitudes de ambos strings, esto para averiguar cual es mayor
+        minimo = len(s2) #Si la longitud del primer string resulta mayor, se guarda el otro string como el más pequeño
+        no_minimo = s1 #El otro string que es más largo queda guardado en otra variable que indica que es el más largo
     else:
-        minimo = len(s1)
-        no_minimo = s2
+        minimo = len(s1) #Si la longitud del segundo string resulta igual o mayor, se guarda el primer string como el más pequeño
+        no_minimo = s2 #Y el string dos queda guardado como el string más largo
 
-    resultado = ""
-    for posicion_string in range(minimo):
-        resultado = resultado + (s1[posicion_string] + s2[posicion_string])
-    resultado = f"{resultado}{no_minimo[minimo: ]}"
-    return resultado
+    resultado = "" #Se guarda una variable con un string vacío, en donde colocaremos nuestro string intercalado
+    for posicion_string in range(minimo): #Se abre un ciclo que ocurrirá la cantidad de veces que sea igual a la longitud del string minimo
+        resultado = resultado + (s1[posicion_string] + s2[posicion_string]) #Cada vez que pase el ciclo, se le suma al resultado la posición del string en el que estén de ambos strings
+    #Por ejemplo, si van por el número 0, se colocará el caracter en la posición número 0 de ambos strings, se suman, y se agregan a la variable de resultado
+    resultado = f"{resultado}{no_minimo[minimo: ]}" #Al final del ciclo, el resultado queda un string que suma el resultado del ciclo y el resto del string que era más largo
+    return resultado #Se devuelve la variable resultado con los arreglos hechos al final del ciclo.
 
 # ── No modifiques debajo de esta línea ──────────────────
 tests = [
